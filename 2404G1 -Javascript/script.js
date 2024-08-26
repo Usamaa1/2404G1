63ca0e1801dee6eea5bcb007229901ae4dde94f9
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-08-26 20:53:33 +0500

    If Else statement completed with Array of Object

---
+++ 2404G1 -Javascript/script.js
@@ -0,0 +1,511 @@
+// alert("Hello World!");
+// console.log("Hello World");
+
+
+// prompt("Enter your Name")
+
+
+
+// let aa;
+// let a;
+// const a;
+
+
+
+
+
+// Operator Precedence
+// let num1 = 10;
+// let num2 = "2";
+
+// let result = num1 + num2;
+
+// console.log(result)
+
+// let str1 = "10"
+// let str2 = "30";
+// let str3 = "2";
+
+// console.log(str1 + str3 - str2)
+
+
+// console.log(10-(2+20))
+
+
+
+// let lastName;
+// let firstName;
+// let date_of_birth;
+// let dateOfBirth;
+// let DateOfBirth;
+// let _jfkaj
+// let &jkfasdjk;
+// let -aklkla;
+// let $jaksjfkjas;
+// let kjsakj23jfskdj;
+
+// let 90usama
+// let #jfsdkjksd;
+
+
+// let a;
+// let A;
+
+
+
+
+
+// let a = 12;
+// let b = 34;
+
+// a = 23
+// console.log(a)
+// console.log(b)
+
+// let num1 = prompt("Enter num1")
+// let num2 = prompt("Enter num2")
+
+
+// document.write((+num1) + (+num2) + "<br>")
+// document.write((+num1) - (+num2) + "<br>")
+// document.write((+num1) * (+num2) + "<br>")
+// document.write((+num1) / (+num2) + "<br>")
+
+
+
+
+
+// console.log(parseInt(num1))
+// console.log(parseFloat(num1))
+
+// console.log(+num1)
+// console.log(num2)
+// console.log((+num1) + (+num2))
+
+
+
+
+// let age;
+
+// age = ['Zahid','Aslam','Ashraf','Malik','Salman','Taimoor','Mustijab']
+
+// let persons = ['Aslam','Riaz',67,'Karachi','Male','Pakistan','BSSE','Labour Manager',true]
+
+
+// let isAlive = false
+// console.log(isAlive)
+
+// console.log(persons[6])
+
+
+// console.table(persons)
+
+
+// console.log(age);
+// document.write(age)
+
+
+// console.log(persons)
+
+
+
+
+// let persons = [
+//     {
+//     firstName: 'Aslam',
+//     lastName: 'Riaz',
+//     age: 64,
+//     city: 'Karachi',
+//     gender: 'Male',
+//     country: 'Pakistan',
+//     education: 'BSSE',
+//     job: 'Labour Manager',
+//     isAlive: true
+// },{
+//     firstName: 'Zahid',
+//     lastName: 'Khan',
+//     age: 56,
+//     city: 'Islamabad',
+//     gender: 'Male',
+//     country: 'Pakistan',
+//     education: 'Fashion Designing',
+//     job: 'Fashion Designer',
+//     isAlive: true
+// },{
+//     firstName: 'Ashraf',
+//     lastName: 'Fakhir',
+//     age: 25,
+//     city: 'Lahore',
+//     gender: 'Male',
+//     country: 'Pakistan',
+//     education: 'BEE',
+//     job: 'Peon',
+//     isAlive: false
+// },
+// {
+//     firstName: 'Muskan',
+//     lastName: 'Mansoor',
+//     age: 22,
+//     city: 'Karachi',
+//     gender: 'Female',
+//     country: 'Pakistan',
+//     education: 'HDSE',
+//     job: 'SEO Expert',
+//     isAlive: true
+// },{
+//     firstName: 'Nimra',
+//     lastName: 'Jilani',
+//     age: 64,
+//     city: 'Karachi',
+//     gender: 'Female',
+//     country: 'Pakistan',
+//     education: 'ADSE',
+//     job: null,
+//     isAlive: true
+// }
+
+// ]
+
+// console.log(persons)
+// console.table(persons)
+// // console.error('jfdskfjksdjkfj')
+// document.write(persons)
+
+
+
+//LOOPS START
+
+// for(let i =0; i < persons.length;i++)
+//     {
+//         console.log(persons[i].firstName,persons[i].lastName,persons[i].gender,persons[i].age)
+//         // console.log(persons[i].lastName)
+//         // console.log(persons[i].gender)
+//         // console.log(persons[i].age)
+//     }
+
+
+// let names = ['Armin','Zahid','Omair','Fahad','Anas','Umair','Imran'];
+
+
+// let names = [];
+
+
+//     for(let i =0; i<names.length;i++)
+//     {
+//         console.log(i,names[i])
+//     }
+
+// console.log(names)
+// document.write(names[0] + "<br>")
+// document.write(names[1]  + "<br>")
+// document.write(names[2]  + "<br>")
+// document.write(names[3]  + "<br>")
+// document.write(names[4]  + "<br>")
+// document.write(names[5]  + "<br>")
+// document.write(names[6]  + "<br>")
+// document.write(names[1]  + "<br> <a href='#'>click me</a>")
+
+
+
+// Some more useful Operators
+
+// INCREMENT OPERATOR
+// a++              //----------> postfix increment
+// ++a              //----------> prefix increment
+// DECREMENT OPERATOR
+// a--              //----------> postfix decrement
+// --a              //----------> prefix decrement
+
+// ----------------------------------------------------
+// a+=2
+// a-=2
+// a*=2
+// a/=2
+
+
+// for
+// while
+// do while
+
+
+// console.log("Length",names.length)
+
+
+// document.write(names[0] + "<br>")
+// document.write(names[1]  + "<br>")
+// document.write(names[2]  + "<br>")
+// document.write(names[3]  + "<br>")
+// document.write(names[4]  + "<br>")
+// document.write(names[5]  + "<br>")
+// document.write(names[6]  + "<br>")
+
+
+
+
+
+
+
+
+
+
+// for(let index = 0; index < names.length; index++)
+//     {
+//         document.write(names[index] + "<br>")
+//     }
+
+
+// for(let i = 0; i < names.length; i++)
+// {
+//     document.write(names[i] + "<br>")
+// }
+
+
+
+
+
+// for(let i=1; i <= 10; i++)
+//     {
+//         console.log(`3 X ${i} = ${i*3}`)
+//     }
+
+
+
+
+// while loop
+
+
+
+
+// let i =0;
+
+// while(i < names.length)
+//     {
+//         console.log(names[i]);
+//         i++
+//     }
+
+
+// for(;i<names.length;)
+//     {
+//         console.log(names[i]);
+
+//     }
+
+
+
+// do{
+//     console.log(names[i]);
+//     i++
+// }
+// while(i < names.length);
+
+
+
+// let userInpt;
+
+// do
+// {
+//     userInpt = prompt('Enter your Value');
+//     console.log(userInpt)
+// }
+// while(userInpt == '')
+
+
+
+
+
+
+
+
+// MAP
+
+// names.map(item => console.log(item))
+
+// names.map((a,b) => console.log(b,a))
+
+
+
+
+
+
+//LOOPS ENDS
+
+
+
+
+
+
+// **********************
+// Conditional Statements
+// **********************
+
+// if(H3>=10,'Pass','Fail')
+
+
+// let userPrompt = prompt('Enter your gender');
+
+
+// if(userPrompt == 'female')
+//     {
+//         console.log('You are allowed in the Beauty Parlor 💇🏻‍♀️💇🏻‍♀️');
+//     }
+//     else
+//     {
+//         console.log('Sorry! You are not allowed in the Beauty Parlor 🤚🏻');
+//     }
+
+
+
+
+
+
+// let userAge = prompt('Enter your age:');
+
+
+
+
+// if (userAge > 200) {
+
+//     console.log('Kindly enter valid Age')
+// }
+// else if (userAge >= 100) {
+//     console.log('Sorry! you are over aged');
+// }
+// else if (userAge >= 18) {
+//     console.log('Hurrray🎉✨! You are eligible for Driving License 🚗🚗🚗')
+// }
+// else if(userAge < 18) {
+//     console.log('Sorry😥! You are not eligible for Driving License 🚗🚗🚗')
+// }
+// else{
+//     console.log('Kindly enter valid Age')
+// }
+
+
+
+
+
+
+
+// let apple = 12;
+
+
+// === ---------------> for checking datatype
+// ==  ---------------> for only checking condition
+
+
+
+// if(apple === 12)
+//     {
+//         console.log('Mein khareed lounga')
+//     }
+//     else
+//     {
+//         console.log('Mein shabaz sharif houn');
+//     }
+
+
+
+
+
+
+
+
+
+
+let persons = [
+    {
+    firstName: 'Aslam',
+    lastName: 'Riaz',
+    age: 64,
+    city: 'Karachi',
+    gender: 'Male',
+    country: 'Pakistan',
+    education: 'BSSE',
+    job: 'Labour Manager',
+    isAlive: true
+},{
+    firstName: 'Zahid',
+    lastName: 'Khan',
+    age: 56,
+    city: 'Islamabad',
+    gender: 'Male',
+    country: 'Pakistan',
+    education: 'Fashion Designing',
+    job: 'Fashion Designer',
+    isAlive: true
+},{
+    firstName: 'Ashraf',
+    lastName: 'Fakhir',
+    age: 25,
+    city: 'Lahore',
+    gender: 'Male',
+    country: 'Pakistan',
+    education: 'BEE',
+    job: 'Peon',
+    isAlive: false
+},
+{
+    firstName: 'Muskan',
+    lastName: 'Mansoor',
+    age: 22,
+    city: 'Karachi',
+    gender: 'Female',
+    country: 'Pakistan',
+    education: 'HDSE',
+    job: 'SEO Expert',
+    isAlive: true
+},{
+    firstName: 'Nimra',
+    lastName: 'Jilani',
+    age: 64,
+    city: 'Karachi',
+    gender: 'Female',
+    country: 'Pakistan',
+    education: 'ADSE',
+    job: null,
+    isAlive: true
+}
+
+]
+
+
+
+
+let isUserExist = false;
+
+let userSearch = prompt('Enter Name');
+
+
+
+for (let i = 0; i < persons.length; i++) {
+
+        if(persons[i].firstName.toLowerCase == userSearch.toLowerCase)
+        {
+            isUserExist = true;
+            break;
+        }
+        else
+        {
+            isUserExist = false;
+        }
+    
+}
+
+
+
+
+
+if(isUserExist)
+{
+        console.log('Yes I Found your name')
+}
+else
+{
+    console.log("Sorry! I didn't Found your name");
+}
+
+
+
+
+
